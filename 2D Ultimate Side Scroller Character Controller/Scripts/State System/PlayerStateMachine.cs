0db4dfd6cad57ad4f46cff6f10918979a08a5eeb
AbdyTekin <aabdullahtekin@gmail.com>
2023-05-10 12:21:16 +0300

    Upload for main content

---
+++ 2D Ultimate Side Scroller Character Controller/Scripts/State System/PlayerStateMachine.cs
@@ -0,0 +1,21 @@
+[System.Serializable]
+public class PlayerStateMachine
+{
+    // Declaration of current runtime state
+    public MainState CurrentState { get; private set; }
+
+    // Function to initialize starting state
+    public void Initialize(MainState startingState)
+    {
+        CurrentState = startingState;
+        CurrentState.Enter();
+    }
+
+    // Function to change current state
+    public void ChangeState(MainState newState)
+    {
+        CurrentState.Exit();
+        CurrentState = newState;
+        CurrentState.Enter();
+    }
+}
